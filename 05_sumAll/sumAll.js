070dfa08890368f785d06d723ff69146addd4e84
bmoran17 <brendamoran1999@gmail.com>
2023-02-20 23:55:00 -0800

    #6 & #7 passed all tests; need to condense code for #7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,20 @@
 const sumAll = function(num1, num2) {
-    const numArray = [];
-    for(let i = num1; i <= num2; i++) {
-        numArray.push(i);
-    }
-    console.log(numArray)
     let total = 0;
-    for(const num of numArray){
-        total += num;
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+    if (num1 < 0 || num2 < 0) return "ERROR";
+
+    if (num1 > num2) {
+        const largerNum = num1
+        num1 = num2
+        num2 = largerNum
+    }
+
+    // adding all numbers in btw
+    for(let i = num1; i <= num2; i++) {
+        total += i
     }
     return total
-};
-
+}
 // Do not edit below this line
 module.exports = sumAll;
