5040c27bd9e1747446c3854d47f1fd0474bf6fcb
bmoran17 <brendamoran1999@gmail.com>
2023-02-23 16:07:26 -0800

    #7, #8, #11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const titles = array.map(function (book) {
+        return book.title;
+    })
+    return titles;
 };
 
 // Do not edit below this line
